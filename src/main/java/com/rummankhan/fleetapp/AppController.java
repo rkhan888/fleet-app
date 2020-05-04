909f5a21982dfc8d72ac73f215b89b596bd41989
rumman <us.rkhan129@gmail.com>
2020-05-03 22:52:02 -0400

    added html templates

---
+++ src/main/java/com/rummankhan/fleetapp/AppController.java
@@ -0,0 +1,15 @@
+package com.rummankhan.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+		
+	}
+
+}
